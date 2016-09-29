b1a4959bc50025abfc97d340c5f08e85816b6b79
Whitney Murray <wnksmith92@yahoo.com>
2016-09-28 21:06:26 -0500

    routing to correct pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+$(document).ready(function() {
+var Router = Backbone.Router.extend({
+	routes: {
+		'board': 'goBoard',
+		'settings': 'goSettings',
+		'play': 'goPlay'
+	},
+	goBoard: function(){
+		$('section').hide();
+		$('#boardScreen').show();
+	},
+	goSettings: function(){
+		$('section').hide();
+		$('#settingsScreen').show();
+	},
+	goPlay: function(){
+		$('section').hide();
+		$('#playScreen').show();
+	}
+})
+	
+	var game = new Router();
+	Backbone.history.start();
+
+});
