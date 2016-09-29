9c706d67ee0ff85d6b68e58c8d47d570411303d4
Whitney Murray <wnksmith92@yahoo.com>
2016-09-29 16:25:39 -0500

    trying to fix loading screen timeout

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,29 +1,43 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
-$(document).ready(function () {
-	var Router = Backbone.Router.extend({
-		routes: {
-			'board': 'goBoard',
-			'settings': 'goSettings',
-			'play': 'goPlay'
-		},
-		goBoard: function goBoard() {
-			$('section').hide();
-			$('#boardScreen').show();
-		},
-		goSettings: function goSettings() {
-			$('section').hide();
-			$('#settingsScreen').show();
-		},
-		goPlay: function goPlay() {
-			$('section').hide();
-			$('#playScreen').show();
-		}
-	});
-
-	var game = new Router();
-	Backbone.history.start();
+var Router = Backbone.Router.extend({
+	routes: {
+		' ': 'goLoad',
+		'load': 'goLoad',
+		'menu': 'goMenu',
+		'board': 'goBoard',
+		'settings': 'goSettings',
+		'play': 'goPlay'
+	},
+	goLoad: function goLoad() {
+		$('section').hide();
+		setTimeout(function () {
+			$('#loadScreen').hide();
+		}, 4000);
+	},
+	goMenu: function goMenu() {
+		$('#loadScreen').hide();
+		$('#boardScreen').hide();
+		$('#settingsScreen').hide();
+		$('#playScreen').hide();
+		$('#menuScreen').show();
+	},
+	goBoard: function goBoard() {
+		$('section').hide();
+		$('#boardScreen').show();
+	},
+	goSettings: function goSettings() {
+		$('section').hide();
+		$('#settingsScreen').show();
+	},
+	goPlay: function goPlay() {
+		$('section').hide();
+		$('#playScreen').show();
+	}
 });
+
+var game = new Router();
+Backbone.history.start();
 
 },{}]},{},[1])
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,25 @@
 'use strict';
-$(document).ready(function() {
 var Router = Backbone.Router.extend({
 	routes: {
+		' ': 'goLoad',
+		'load': 'goLoad',
+		'menu':'goMenu',
 		'board': 'goBoard',
 		'settings': 'goSettings',
 		'play': 'goPlay'
+	},
+	goLoad: function(){
+		$('section').hide();
+	setTimeout(function() {
+		$('#loadScreen').hide();
+	}, 4000);
+	},
+	goMenu: function(){
+		$('#loadScreen').hide();
+		$('#boardScreen').hide();
+		$('#settingsScreen').hide();
+		$('#playScreen').hide();
+		$('#menuScreen').show();
 	},
 	goBoard: function(){
 		$('section').hide();
@@ -18,9 +33,8 @@ var Router = Backbone.Router.extend({
 		$('section').hide();
 		$('#playScreen').show();
 	}
-})
+});	
 	
 	var game = new Router();
 	Backbone.history.start();
 
-});
